6eabefa9973344f1009cc1d195acb1f9836876a6
Saboor Bakshi <saboor.bakshi@icloud.com>
2025-01-25 17:08:36 -0500

    added cat pics

--- src/app/cats/page.tsx
+++ src/app/cats/page.tsx
@@ -3,8 +3,20 @@ import Image from 'next/image'
 export default function Cats() {
   return (
     <div>
-      <div className="text-[26px]">Cats</div>
-      <div>A log of all the cats cared for back home.</div>
+      <div className="mb-4">
+        <div className="text-[26px]">Cats</div>
+      </div>
+      <div className='flex gap-2'>
+        <div className="w-1/2 flex flex-col gap-2">
+          <img src="cats/panda.png" />
+          <img src="cats/junior.png" />
+        </div>
+        <div className="w-1/2 flex flex-col gap-2">
+          <img src="cats/cat1.png" />
+          <img src="cats/cat2.png" />
+        </div>
+      </div>
+      <div></div>
     </div>
   )
 }
