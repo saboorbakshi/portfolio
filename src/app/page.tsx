aa6737c3e0758af81fee89bad360179a0bd2849b
Saboor Bakshi <saboor.bakshi@icloud.com>
2025-01-25 12:18:42 -0500

    added email to contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,15 @@ export default function Home() {
         <div>Software Engineer with a deep appreciation for great design.</div>
         <div>Studying Computer Science at the University of Waterloo.</div>
         <div className="flex gap-2 mt-4">
-          <Link label="Twitter" url="https://twitter.com/saboorbakshi" />
+          <Link label="X" url="https://twitter.com/saboorbakshi" />
           <div>•</div>
           <Link label="Github" url="https://github.com/saboorbakshi" />
           <div>•</div>
           <Link label="LinkedIn" url="https://linkedin.com/in/saboor-bakshi" />
           <div>•</div>
           <Link label="Resume" url="/saboor-bakshi-resume.pdf" />
+          <div>•</div>
+          <Link label="Email" url="mailto:saboor.bakshi@uwaterloo.ca" />
         </div>
       </div>
 
