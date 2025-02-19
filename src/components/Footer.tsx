7ad01e6fcb9dacfd99297d227afb022c97ffda48
Saboor Bakshi <saboor.bakshi@icloud.com>
2025-02-19 07:15:15 -0500

    style: increase footer text size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,16 +2,16 @@ export default function Footer() {
   return (
     <footer className="flex flex-col items-center justify-center px-6">
       <div className="w-full max-w-screen-custom flex justify-between items-center py-3">
-        <a href="/" className="text-sm sm:text-[15px] text-fgSecondary hover:underline">
+        <a href="/" className="text-fgSecondary hover:underline">
           Saboor Bakshi
         </a>
 
         <div className="flex items-center gap-1">
-          <div className="relative flex items-center justify-center p-1.5">
+          <div className="relative flex items-center justify-center p-1">
             <div className="absolute size-2 animate-ping rounded-full bg-green-500 opacity-75"></div>
             <div className="size-2 rounded-full bg-green-500"></div>
           </div>
-          <div className="text-[15px] text-fgSecondary">Updated Feb 19, 2025</div>
+          <div className="text-fgSecondary">Updated Feb 19, 2025</div>
         </div>
       </div>
     </footer>
