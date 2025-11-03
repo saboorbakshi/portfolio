4a768188b12de474879658338cf245bf14afffa0
saboorbakshi <saboor.bakshi@uwaterloo.ca>
2025-11-03 03:50:26 -0500

    fix: not found hero page path

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center">
-      <Image src="/scenary.png" alt="" width={600} height={600} />
+      <Image src="/scenary.webp" alt="" width={600} height={600} />
       <h1 className="pt-10 pb-4">
         We could not find the page you were looking for.
       </h1>
